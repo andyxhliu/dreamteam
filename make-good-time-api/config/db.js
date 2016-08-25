4745f5e6bdc6de9da7fe55b438a586d199f319da
Kaitlyn Tierney <k@krtierney.com>
2016-08-25 21:08:38 +0100

    Set up for deployment

--- make-good-time-api/config/db.js
+++
@@ -1,9 +0,0 @@
-dbURIs = {
-  test: "mongodb://localhost/make-good-time-api-test",
-  development: "mongodb://localhost/make-good-time-api",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/make-good-time-api"
-}
-
-module.exports = function(env) {
-  return dbURIs[env];
-}
--- make-good-time-api/config/token.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: "dkczDBSIsdcUCsdsdOY73Y8EzczP239Esc8UWDCUsdHsdcNSDKVpcsWDC8W"
-}
