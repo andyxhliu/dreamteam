5a0e1c6ddfb28eaf1d7e9de6edd59a2d172c1a35
Kaitlyn Tierney <k@krtierney.com>
2016-08-25 21:09:12 +0100

    Adding files

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+var User = require("../models/user");
+var Place = require('../models/place');
+
+function me(req, res) {
+  User.findById(req.user._id).populate('favorites').exec(function(err, user) {
+    if(err) return res.status(500).json(err);
+    return res.status(200).json(user);
+  });
+}
+
+function favorite(req, res) {
+  console.log("this is the favorite user..", req.user);
+  // find favorite by place_id
+  Place.findOne({ placeId: req.params.placeId })
+    .then(function(place) {
+      if(!place) {
+        return Place.create(req.body)
+      }
+      return new Promise(function(resolve, reject) { resolve(place); });
+    })
+    .then(function(place) {
+      return User.findById(req.user._id)
+        .then(function(user) {
+
+          var idx = user.favorites.indexOf(place._id.toString());
+
+          if(idx === -1) {
+            user.favorites.push(place);
+          } else {
+            user.favorites.splice(idx, 1);
+          }
+
+          return user.save();
+        });
+    })
+    .then(function(user) {
+      res.status(200).json(user);
+    })
+    .catch(function(err) {
+      res.status(500).json(err);
+    });
+};
+
+module.exports = {
+  favorite: favorite,
+  me: me
+}
